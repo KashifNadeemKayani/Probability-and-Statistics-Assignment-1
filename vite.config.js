a6c4f27b443059e58738ba7bfb19baefa860be7d
Kashif Nadeem Kayani <kayanikashif970@gmail.com>
2025-02-22 15:38:50 +0500

    yml added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Allows access from any device on the network
-    port: 5173,       // Default port (change if needed)
-    strictPort: true, // Ensures it uses the specified port
-  }
+  base: "/StatsA1/"
 })
