d1c501630bc5639077b19a996c0dd7e84b06ddf7
Kashif Nadeem Kayani <kayanikashif970@gmail.com>
2025-03-05 21:38:55 +0500

    corrected and finalized

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/StatsA1/"
-})
+  base: '/StatsA1/',
+  server: {
+    host: true, // Allows access from the local network
+    port: 5173, // Default Vite port, you can change if needed
+  },
+});
