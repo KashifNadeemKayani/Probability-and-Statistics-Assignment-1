156f4cd0886f12531bd95f05878a54c9cc14d918
Kashif Nadeem Kayani <kayanikashif970@gmail.com>
2025-03-05 21:42:53 +0500

    namechanged

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/StatsA1/',
+  base: '/Probability-and-Statistics-Assignment-1/',
   server: {
     host: true, // Allows access from the local network
     port: 5173, // Default Vite port, you can change if needed
